b764943ea9b7f9e81e3b7d8a9122ebcec3ad357a
dudb <1797890817@qq.com>
2016-10-28 14:48:04 +0800

    ADD ExceptionDemo
    Signed-off-by: dudb <1797890817@qq.com>

---
+++ JavaCore/src/com/ddb/javacore/exception/ExceptionDemo.java
@@ -0,0 +1,9 @@
+package com.ddb.javacore.exception;
+
+public class ExceptionDemo {
+
+	public static void main(String[] args) {
+		System.out.println(args[2]);
+	}
+
+}
---
+++ JavaCore/src/com/ddb/javacore/exception/ExceptionDemo4.java
@@ -0,0 +1,29 @@
+package com.ddb.javacore.exception;
+
+public class ExceptionDemo4 {
+
+	public static void main(String[] args) {
+		//PrintStream out = System.out;
+
+		int v3 = 0;
+		try {
+			int v1 = Integer.parseInt(args[0]);
+			int v2 = Integer.parseInt(args[1]);
+			v3 = v1 / v2;
+			return;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("我现在捕获到数组越界异常，我打印的堆栈信息如下：");
+			e.printStackTrace();
+			return;
+		} catch (ArithmeticException e) {
+			System.out.println("我现在捕获到算法异常---除数为0，我打印的堆栈信息如下：");
+			e.printStackTrace();
+			return;
+		}finally {
+			System.out.println("无论错误或者异常如何，我都 会被执行！----我是finally 语句块 ");
+			System.out.println();
+			System.out.println("v3 = " + v3);
+		}
+	}
+
+}
