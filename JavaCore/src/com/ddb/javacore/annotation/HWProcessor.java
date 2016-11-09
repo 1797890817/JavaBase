1623053c4ec09c02a37f07395089ef09d70f8a58
dudb <1797890817@qq.com>
2016-11-09 16:22:58 +0800

    Add Annotation Demo

---
+++ JavaCore/src/com/ddb/javacore/annotation/HWProcessor.java
@@ -0,0 +1,19 @@
+package com.ddb.javacore.annotation;
+
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.TypeElement;
+
+public class HWProcessor extends AbstractProcessor {
+
+	@Override
+	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+		// TODO Auto-generated method stub
+	
+		
+		return false;
+	}
+
+}
